dea1e587e5c38b1e4faa00e99ec97a6cc6276435
Kshitij Khule <kshitijg.khule@gmail.com>
2026-03-19 18:34:36 +0530

    Skip database init during test

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,10 +1,15 @@
 import pytest
 from app import create_app
+import os
 
 @pytest.fixture
 def client():
+    # Tell the app we're testing
+    os.environ['TESTING'] = 'true'
+    
     app = create_app()
     app.config['TESTING'] = True
+    
     with app.test_client() as client:
         yield client
 
